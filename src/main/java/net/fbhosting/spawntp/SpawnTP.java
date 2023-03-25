79945d6ae8aaa15777d86506b7c1b2f08502cf99
Malte Linke <me@parzival.space>
2023-03-26 00:20:33 +0100

    implemented plugin logic

--- src/main/java/net/fbhosting/spawntp/SpawnTP.java
+++ src/main/java/net/fbhosting/spawntp/SpawnTP.java
@@ -1,20 +1,31 @@
 package net.fbhosting.spawntp;
 
-import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import net.fbhosting.spawntp.commands.SetSpawnCommand;
+import net.fbhosting.spawntp.commands.SpawnCommand;
 
 @Slf4j
 public final class SpawnTP extends JavaPlugin implements Listener {
+  @Getter
+  private static SpawnTP instance;
 
   @Override
   public void onEnable() {
+    instance = this; // NOSONAR - intended
+    
     // load default configuration
     saveDefaultConfig();
 
     // register commands
+    getCommand("spawn").setPermission("spawntp.teleport");
+    getCommand("spawn").setExecutor(new SpawnCommand());
+    
+    getCommand("setspawn").setPermission("spawntp.admin");
+    getCommand("setspawn").setExecutor(new SetSpawnCommand());
   }
 
   @Override
